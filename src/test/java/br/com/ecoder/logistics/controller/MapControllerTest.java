72fcbed32224b5dd816e6b3ae7c1d87378e159a6
aalvares <aalvares@uolinc.com>
2014-09-24 16:57:41 -0300

    Sistema de malha logistica.

--- src/test/java/br/com/ecoder/logistics/controller/MapControllerTest.java
+++ src/test/java/br/com/ecoder/logistics/controller/MapControllerTest.java
@@ -1,5 +1,93 @@
 package br.com.ecoder.logistics.controller;
 
+import static org.mockito.Matchers.anyFloat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.LinkedList;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.springframework.http.ResponseEntity;
+
+import br.com.ecoder.logistics.application.MapApplication;
+import br.com.ecoder.logistics.model.Map;
+import br.com.ecoder.logistics.model.Point;
+import br.com.ecoder.logistics.model.Route;
+
 public class MapControllerTest {
 
+    @Mock
+    MapApplication application;
+
+    MapController controller;
+
+    Map map;
+
+    @Before
+    public void init() {
+
+        MockitoAnnotations.initMocks(this);
+
+        map = new Map();
+        map.setName("test");
+        map.setAutonomy(10.0f);
+        map.setGasCost(2.5f);
+        map.setTotalCost(6.25f);
+        map.setRoutes(new LinkedList<Route>());
+
+        Route route = new Route();
+        route.setOrigin(new Point());
+        route.setDestiny(new Point());
+        route.setDistance(1);
+        route.getOrigin().setName("A");
+        route.getDestiny().setName("B");
+
+        map.getRoutes().add(route);
+
+        when(application.findByName(anyString())).thenReturn(map);
+        when(application.findRoute(anyString(), anyString(), anyString(), anyFloat(), anyFloat())).thenReturn(map);
+
+        controller = new MapController();
+        Whitebox.setInternalState(controller, "application", application);
+    }
+
+    @Test
+    public void should_call_persistence() {
+
+        Map map = new Map();
+
+        controller.save(map);
+
+        Mockito.verify(application, Mockito.times(1)).persist(map);
+    }
+
+    @Test
+    public void should_call_find() {
+
+        ResponseEntity<Map> response = controller.get("test");
+
+        Mockito.verify(application, Mockito.times(1)).findByName("test");
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getBody());
+        Assert.assertNotNull(response.getBody().getName());
+        Assert.assertEquals(map, response.getBody());
+    }
+
+    @Test
+    public void should_call_route() {
+
+        ResponseEntity<Map> response = controller.route("test", "A", "B", 10.0f, 2.5f);
+
+        Mockito.verify(application, Mockito.times(1)).findRoute("test", "A", "B", 10.0f, 2.5f);
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getBody());
+        Assert.assertNotNull(response.getBody().getName());
+        Assert.assertEquals(map, response.getBody());
+    }
 }

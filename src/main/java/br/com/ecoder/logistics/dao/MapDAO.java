1b932955ecd25737a71b5a28765210c4cfb8453f
aalvares <aalvares@uolinc.com>
2014-09-24 20:20:59 -0300

    SAC-432 Integrar o validation.js com o SAC Templates

--- src/main/java/br/com/ecoder/logistics/dao/MapDAO.java
+++ src/main/java/br/com/ecoder/logistics/dao/MapDAO.java
@@ -1,5 +1,14 @@
 package br.com.ecoder.logistics.dao;
 
+import java.util.List;
+
+import br.com.ecoder.logistics.model.Route;
+
+
 public interface MapDAO {
+
+    public void createRelationship(String map, String origin, String destiny, Float distance);
+
+    public List<Route> findRoute(String map, String origin, String destiny);
 
 }
--- src/main/java/br/com/ecoder/logistics/dao/impl/MapNeo4jDAO.java
+++ src/main/java/br/com/ecoder/logistics/dao/impl/MapNeo4jDAO.java
@@ -1,10 +1,140 @@
 package br.com.ecoder.logistics.dao.impl;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.neo4j.graphalgo.CommonEvaluators;
+import org.neo4j.graphalgo.CostEvaluator;
+import org.neo4j.graphalgo.GraphAlgoFactory;
+import org.neo4j.graphalgo.PathFinder;
+import org.neo4j.graphalgo.WeightedPath;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipExpander;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.index.IndexService;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+import org.neo4j.kernel.Traversal;
+import org.neo4j.util.GraphDatabaseLifecycle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import br.com.ecoder.logistics.dao.MapDAO;
+import br.com.ecoder.logistics.model.Route;
 
 @Service
 public class MapNeo4jDAO implements MapDAO {
 
+    public enum MyDijkstraTypes implements RelationshipType {
+        REL
+    }
+
+    public class MapRepository {
+
+        public EmbeddedGraphDatabase graphDb = null;
+        public GraphDatabaseLifecycle lifecycle = null;
+        public IndexService index = null;
+
+        public MapRepository(String name) {
+
+            graphDb = new EmbeddedGraphDatabase(name);
+            lifecycle = new GraphDatabaseLifecycle(graphDb);
+            lifecycle.addLuceneIndexService();
+            index = lifecycle.indexService();
+        }
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(MapNeo4jDAO.class);
+    private static final String COST = "cost";
+    private static final String NAME = "name";
+
+    private static final java.util.Map<String, MapRepository> repositories = new java.util.HashMap<String, MapNeo4jDAO.MapRepository>();
+
+    private static final RelationshipExpander expander;
+    private static final CostEvaluator<Double> costEvaluator;
+    private static final PathFinder<WeightedPath> dijkstraPathFinder;
+
+    static {
+        // set up path finder
+        expander = Traversal.expanderForTypes(MyDijkstraTypes.REL, Direction.BOTH);
+        costEvaluator = CommonEvaluators.doubleCostEvaluator(COST);
+        dijkstraPathFinder = GraphAlgoFactory.dijkstra(expander, costEvaluator);
+    }
+
+    public MapNeo4jDAO() {}
+
+    @Override
+    public void createRelationship(String map, String origin, String destiny, Float distance) {
+
+        MapRepository repository = getMapRepository(map);
+        Transaction tx = repository.graphDb.beginTx();
+
+        try {
+
+            // find/create nodes
+            Node firstNode = findOrCreateNode(repository, origin);
+            Node secondNode = findOrCreateNode(repository, destiny);
+
+            // add relationship
+            Relationship rel = firstNode.createRelationshipTo(secondNode, MyDijkstraTypes.REL);
+            rel.setProperty(COST, distance);
+
+            tx.success();
+
+        } catch (Exception ex) {
+            logger.error("", ex);
+        } finally {
+            tx.finish();
+        }
+    }
+
+    private MapRepository getMapRepository(String map) {
+
+        MapRepository repository = repositories.get(map);
+
+        if (repository == null) {
+            repository = new MapRepository(map);
+            repositories.put(map, repository);
+        }
+
+        return repository;
+    }
+
+    private Node findOrCreateNode(MapRepository repository, String nodeName) {
+
+        Node node = getNode(repository, nodeName);
+
+        if (node == null) {
+            node = repository.graphDb.createNode();
+            node.setProperty(NAME, nodeName);
+            repository.index.index(node, NAME, node.getProperty(NAME));
+        }
+
+        return node;
+    }
+
+    private Node getNode(MapRepository repository, String name) {
+        return repository.index.getSingleNode(NAME, name);
+    }
+
+    @Override
+    public List<Route> findRoute(String map, String origin, String destiny) {
+
+        MapRepository repository = getMapRepository(map);
+        List<Route> result = new LinkedList<Route>();
+
+        Node start = getNode(repository, origin);
+        Node end = getNode(repository, destiny);
+        WeightedPath path = dijkstraPathFinder.findSinglePath(start, end);
+
+        for (Node node : path.nodes()) {
+            System.out.println(node.getProperty(NAME));
+            System.out.println(node.getSingleRelationship(MyDijkstraTypes.REL, Direction.BOTH));
+        }
+
+        return result;
+    }
 }
